10bba747849de944bab12a1408c861978532ccf2
Timothy Barnaby <tbarnaby1@gmail.com>
2022-11-11 13:25:13 -0600

    updated models

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,44 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+
+    },
+    product_name: {
+      type: DataType.STRING,
+      allowNull: false
+
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      //VALIDATES TAHT VALUE IS A DECIMAL
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10
+      //VALIDATES THAT VALUE IS A numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+
+
+    }
   },
   {
     sequelize,
