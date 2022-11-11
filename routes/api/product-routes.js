41cef0c9aab15a0bbc84c7f77a8c17725a29c317
Timothy Barnaby <tbarnaby1@gmail.com>
2022-11-11 13:37:42 -0600

    added routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,15 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const locationData = await Product.findAll();
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
